d88cc8a58a4373bd79c15ea30eb3b4b141b2ed70
Zp07 <zepta2249@gmail.com>
2024-07-26 18:02:07 -0500

    First presentation

---
+++ components/Main.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { ActivityIndicator, View, FlatList } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { getLatestGames } from "../lib/metacritic";
+import { Logo } from "./Logo";
+import { AnimatedGameCard } from "./GameCard";
+
+export function Main() {
+  const [games, setGames] = useState([]);
+  const insets = useSafeAreaInsets();
+
+  useEffect(() => {
+    getLatestGames().then((games) => {
+      setGames(games);
+    });
+  }, []);
+
+  return (
+    <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}>
+      <View style={{ marginTop: 20, marginBottom: 20 }}>
+        <Logo />
+      </View>
+      {games.length === 0 ? (
+        <ActivityIndicator color={"#fff"} size={"large"} />
+      ) : (
+        <FlatList
+          data={games}
+          keyExtractor={(game) => game.slug}
+          renderItem={({ item, index }) => (
+            <AnimatedGameCard game={item} index={index} />
+          )}
+        />
+      )}
+    </View>
+  );
+}
